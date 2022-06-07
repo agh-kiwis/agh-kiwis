0c9052b00cec6bfb53e08761715be272ae6d28e8
Bartosz Szar <szarbartosz@gmail.com>
2022-06-07 23:38:30 +0200

    Redux setup [AK-38] (#3)

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,12 +1,19 @@
-import { AppProps } from 'next/app';
 import './styles.css';
+import { store } from '../redux/store';
+import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Counter } from '../components/Counter/Counter';
+
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
+      <Provider store={store}>
+        <ChakraProvider>
+          <Counter />
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Provider>
     </>
   );
 }
