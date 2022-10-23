d6faa04acacdf47606878f28be4dc12dcdacc090
Bartosz Szar <szarbartosz@gmail.com>
2022-10-23 12:53:47 +0200

    update initial db seed

--- apps/server/src/database/initial-seed.ts
+++ apps/server/src/database/initial-seed.ts
@@ -1,7 +1,7 @@
 // This is used for storing seeding data
 export const InitialSeed = {
   email: 'email@gmail.com',
-  password: 'password123',
+  password: 'password1234',
 
   colors: ['#0077B6', '#1EA896', '#FF8C42', '#E53E3E', '#E0479E', '#ECC94B'],
 };
