1a0674df0f2a3ae7d394feeabf5ee7c658879135
Bartosz Szar <szarbartosz@gmail.com>
2022-11-13 01:05:29 +0100

    update user creation method

--- apps/server/src/database/initial-seed.ts
+++ apps/server/src/database/initial-seed.ts
@@ -3,5 +3,6 @@ export const InitialSeed = {
   email: 'email@gmail.com',
   password: 'password1234',
 
-  colors: ['#0077B6', '#1EA896', '#FF8C42', '#E53E3E', '#E0479E', '#ECC94B'],
+  categories: ['Sleep', 'Meals', 'Sport', 'Study', 'Hobby', 'Other'],
+  colors: ['#0077B6', '#E0479E', '#FF8C42', '#E53E3E', '#1EA896', '#ECC94B'],
 };
--- apps/server/src/users/users.service.ts
+++ apps/server/src/users/users.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { Category } from '../categories/entities/category.entity';
+import { Color } from '../categories/entities/color.entity';
+import { InitialSeed } from '../database/initial-seed';
 import { EntityCondition } from '../utils/types/entity-condition.type';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
@@ -6,8 +9,24 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserInput: CreateUserInput) {
-    return User.create(createUserInput).save();
+  async create(createUserInput: CreateUserInput) {
+    const user: User = await User.create(createUserInput).save();
+
+    for (const color of InitialSeed.colors) {
+      await Color.create({
+        hexCode: color,
+      }).save();
+    }
+
+    InitialSeed.colors.forEach(async (color, index) => {
+      await Category.create({
+        color: await Color.findOne({ where: { hexCode: color } }),
+        name: InitialSeed.categories[index],
+        user: user,
+      }).save();
+    });
+
+    return user;
   }
 
   findOne(fields: EntityCondition<User>) {
