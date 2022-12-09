d779eb99e53ca050dcf5f49de91dbeb7a1a7b395
Bartosz Szar <szarbartosz@gmail.com>
2022-12-09 19:29:52 +0100

    change numeric inputs to readOnly

--- libs/ui-components/src/lib/DependentFields/DependentMinChunkTimeField.tsx
+++ libs/ui-components/src/lib/DependentFields/DependentMinChunkTimeField.tsx
@@ -16,5 +16,5 @@ export const DependentMinChunkTimeField: React.FC<
     setFieldValue(name, `${values.chunking.minChunkTime.minutes}min`);
   }, [name, setFieldValue, values.chunking.minChunkTime.minutes]);
 
-  return <InputField name={name} label="Min chunk itme" />;
+  return <InputField name={name} readOnly={true} label="Min chunk itme" />;
 };
