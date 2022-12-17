1a6866159599e7c7a9ea1a1c4515d456f24cff33
Dima Mikielewicz <dymitrmikielewicz@gmail.com>
2022-12-17 19:42:01 +0100

    Limit chunks to 10

--- apps/server/src/utils/myDate.ts
+++
@@ -1,7 +0,0 @@
-export const newDate = (date: Date) => {
-  //   Set date's month to the previous month
-  date.setMonth(date.getMonth() - 1);
-  // Add a week
-  date.setDate(date.getDate() + 7);
-  return date;
-};
