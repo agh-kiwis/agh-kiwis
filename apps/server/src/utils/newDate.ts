e3601cf1e1d21a62c7c80ae68105c4f45dc58c61
Dima Mikielewicz <dymitrmikielewicz@gmail.com>
2023-01-21 19:58:59 +0100

    Revert adding presentation setup

--- apps/server/src/utils/newDate.ts
+++ apps/server/src/utils/newDate.ts
@@ -4,17 +4,17 @@ const newDate = (
   year: number,
   month: number,
   day: number,
-  hour: number = 0,
+  hour = 0,
   minute = 0
 ) => {
   const date = new Date(year, month - 1, day, hour, minute);
 
-  // const weeksPassed = Math.floor(
-  //   (new Date().getTime() - STARTING_POINT.getTime()) /
-  //     (1000 * 60 * 60 * 24 * 7)
-  // );
+  const weeksPassed = Math.floor(
+    (new Date().getTime() - STARTING_POINT.getTime()) /
+      (1000 * 60 * 60 * 24 * 7)
+  );
 
-  // date.setTime(date.getTime() + (weeksPassed + 1) * 7 * 24 * 60 * 60 * 1000);
+  date.setTime(date.getTime() + (weeksPassed + 1) * 7 * 24 * 60 * 60 * 1000);
 
   return date;
 };
--- apps/server/src/utils/setup.ts
+++ apps/server/src/utils/setup.ts
@@ -1,11 +1,9 @@
 import { INestApplication, Logger } from '@nestjs/common';
 import { seedDatabase } from '../database/db-seeder';
-import { seedDatabasePresentation } from '../database/db-seeder-presentation';
 
 export const setupApp = async (app: INestApplication) => {
   if (process.env.APP_SEED_DATABASE === 'true') {
     Logger.log('Seeding database');
-    // seedDatabase(app);
-    seedDatabasePresentation(app);
+    seedDatabase(app);
   }
 };
